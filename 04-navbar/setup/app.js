64d6ce60fd2814bb7eb0abef512c5c1572791beb
Vinicius Ornelas de Almeida <vinicius.almeida@yandeh.com.br>
2022-03-03 18:10:37 -0300

    Nav bar practice

--- 04-navbar/setup/app.js
+++ 04-navbar/setup/app.js
@@ -3,3 +3,24 @@
 // add - add class
 // remove - remove class
 // toggle - toggles class
+
+const navToggle = document.querySelector(".nav-toggle");
+const links = document.querySelector(".links");
+
+navToggle.addEventListener("click", function () {
+  //    console.log(links.classList);
+  // console.log(links.classList.contains("random"));
+  // console.log(links.classList.contains("links"));
+
+  //   se a classe links também tiver a classe show-links
+  //   if (links.classList.contains("show-links")) {
+  //     //   remove a classe show-links de links
+  //     links.classList.remove("show-links");
+  //   } else {
+  //     //   adiciona a classe show-links
+  //     links.classList.add("show-links");
+  //   }
+
+  //   faz a mesma coisa que o código acima
+  links.classList.toggle("show-links");
+});

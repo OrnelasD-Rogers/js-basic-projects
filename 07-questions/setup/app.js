6404f671e7ac3588e6a2b7d2b1611641201b6fdf
Vinicius Ornelas de Almeida <vinicius.almeida@yandeh.com.br>
2022-03-06 18:56:12 -0300

    Questions and answers

--- 07-questions/setup/app.js
+++ 07-questions/setup/app.js
@@ -1,2 +1,10 @@
+const btns = document.querySelectorAll(".question-btn");
+
 //using selectors inside the element
+btns.forEach(function (btn) {
+  btn.addEventListener("click", function (e) {
+    console.log(e.currentTarget.parentElement);
+  });
+});
+
 // traversing the dom
